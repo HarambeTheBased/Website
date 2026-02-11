bb22d3e749ce190c7187a127ba448d6a14d8de65
GyroBasado <antoniogaitanadra@gmail.com>
2026-02-11 22:52:01 +0100

    Nueva página web

--- js/main.js
+++ js/main.js
@@ -7,3 +7,55 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const reviewsSection = document.querySelector("#reviews");
+
+  if (!reviewsSection) return;
+
+  // Esperar a que Elfsight cargue el contenido
+  const observer = new MutationObserver(() => {
+    const links = reviewsSection.querySelectorAll("a");
+
+    if (links.length > 0) {
+      const lastLink = links[links.length - 1];
+      lastLink.style.display = "none";
+    }
+  });
+
+  observer.observe(reviewsSection, {
+    childList: true,
+    subtree: true
+  });
+});
+
+document.querySelectorAll('.submenu-toggle').forEach(toggle => {
+  toggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const parent = this.closest('.has-submenu');
+    parent.classList.toggle('open');
+  });
+});
+
+let lastScroll = 0;
+
+const topbar = document.querySelector('.topbar');
+const header = document.querySelector('.cabecera');
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll && currentScroll > 50) {
+    // Scroll hacia abajo
+    topbar.classList.add('hidden');
+    header.classList.add('stuck');
+  } else {
+    // Scroll hacia arriba
+    topbar.classList.remove('hidden');
+    header.classList.remove('stuck');
+  }
+
+  lastScroll = currentScroll;
+});
